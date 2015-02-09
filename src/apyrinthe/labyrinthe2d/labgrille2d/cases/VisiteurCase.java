2c3af112bc915ca054ea419834f016a462727dd8
Adrien <ad.duroy@gmail.com>
2015-02-09 07:16:50 +0100

    Mise sous licence LGPL.

--- src/apyrinthe/labyrinthe2d/labgrille2d/cases/VisiteurCase.java
+++ src/apyrinthe/labyrinthe2d/labgrille2d/cases/VisiteurCase.java
@@ -1,3 +1,23 @@
+/*
+Copyright Adrien Duroy (2015)
+
+This file is part of Apyrinthe.
+
+ad.duroy@gmail.com
+
+Apyrinthe is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Apyrinthe is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Apyrinthe.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package apyrinthe.labyrinthe2d.labgrille2d.cases;
 
 import apyrinthe.VisiteurZone;
